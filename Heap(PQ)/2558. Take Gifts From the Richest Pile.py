278df4c7e90d9b0c32f4bc9f322886cf00ef6271
DiggsChow_Mugiwara <133131161+DiggschowMugiwara@users.noreply.github.com>
2024-12-13 13:05:43 +0530

    Update 2558. Take Gifts From the Richest Pile.py

--- Heap(PQ)/2558. Take Gifts From the Richest Pile.py
+++ Heap(PQ)/2558. Take Gifts From the Richest Pile.py
@@ -1,1 +1,15 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        max_heap = []
+        left_over = []
+        for i in range(len(gifts)):
+            heapq.heappush(max_heap,-gifts[i])
+        for i in range(k):
+            x = heapq.heappop(max_heap)
+            x = -x
+            heapq.heappush(max_heap,-int(pow(x,0.5)))
+        return -sum(max_heap)
 
+
+//TIME COMPLEXITY:O(NLOGN)
+//SPACE COMPLEXITY:O(N)
