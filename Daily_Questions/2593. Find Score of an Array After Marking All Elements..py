2b70abee1e85c96aa59bc183c8fddd6364904761
DiggsChow_Mugiwara <133131161+DiggschowMugiwara@users.noreply.github.com>
2024-12-13 12:58:21 +0530

    Update 2593. Find Score of an Array After Marking All Elements..py

--- Daily_Questions/2593. Find Score of an Array After Marking All Elements..py
+++ Daily_Questions/2593. Find Score of an Array After Marking All Elements..py
@@ -1,1 +1,24 @@
 
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        
+        min_heap = []
+        for i in range(len(nums)):
+            heapq.heappush(min_heap,(nums[i],i))
+        score = 0
+        marked = [0]*len(nums)
+        while min_heap:
+            x,index = heapq.heappop(min_heap)
+            if marked[index] == 1:
+                continue
+            marked[index] = 1
+            score += x
+            if index+1 < len(nums):
+                marked[index+1] = 1
+            if index-1 >= 0:
+                marked[index-1] = 1
+        return score
+        
+
+  //TIME COMPLEXITY:O(NLOGN)
+  //SPACE COMPLEXITY:O(N)
