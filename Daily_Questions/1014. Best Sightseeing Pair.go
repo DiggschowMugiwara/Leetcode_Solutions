4419037d73924b1226622d804e11edfc8ec36113
DiggsChow_Mugiwara <133131161+DiggschowMugiwara@users.noreply.github.com>
2024-12-27 22:39:13 +0530

    Create 1014. Best Sightseeing Pair.go

---
+++ Daily_Questions/1014. Best Sightseeing Pair.go
@@ -0,0 +1,25 @@
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func maxScoreSightseeingPair(values []int) int {
+    n := len(values)
+    if n < 2{
+        return -1
+    }
+    maxi := values[0]
+    res := math.MinInt
+    for i := 1;i<len(values);i++{
+        res = max(res,maxi + values[i]-i)
+        maxi = max(maxi,values[i]+i)
+    }
+    return res
+
+
+}
+
+// TIME COMPLEXITY O(N)
+// SPACE COMPLEXITY O(1)
